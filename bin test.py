31fa5b4e06a9488e99ce6744e9dbcd3778475111
JudeGaston <159786667+JudeGaston@users.noreply.github.com>
2024-04-16 16:20:59 +0100

    Update bin test.py

--- bin test.py
+++ bin test.py
@@ -1,5 +1,20 @@
 #bin test
 
 import numpy as np
+from CA2.py import Metro, Accept, H
 
-bin1 = np.zeros(500) 
+B = 100                                
+n = 10000
+x = 2 
+hits = 0  
+n_per_bin = 18
+
+accepted = np.array([])  
+accepted = accepted[1000:]
+
+bins = np.zeros(9000)                  #9000 is the length of accepted after trimming
+for a in range(9000):
+    in_the_bin = 0
+    for b in range(n_per_bin):
+        dx = np.random.uniform(-0.01, 0.01)
+        
